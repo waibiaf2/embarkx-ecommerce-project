10282cc86268c2c307d959cb3d24f6e829533fc8
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-02-07 11:52:06 +0300

    Refactor CategoryController to use field injection for CategoryService

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,11 +10,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
-    private final CategoryService categoryService;
+    /**
+     * Constructor Injection
+     *****************************************/
+    /*private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
-    }
+    }*/
+
+    /****************************************************
+     * Field Injection
+     * *********************/
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/public/categories")
     public List<Category> getCategories() {
