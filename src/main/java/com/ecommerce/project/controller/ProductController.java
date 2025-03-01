fe56634bb07decbde3ee712f8ea087f92b22ba16
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-03-01 10:27:43 +0300

    Implement Add Product
    Add Product creation functionality in ProductController and ProductService

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,10 +1,25 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.config.AppConstants;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(AppConstants.BASE_URL + "/products")
+@RequestMapping(AppConstants.BASE_URL)
 public class ProductController {
+    private final ProductService productService;
+    
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    
+    @PostMapping("/admin/category/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO, @PathVariable Long categoryId) {
+        ProductDTO savedProduct = productService.addProduct(productDTO, categoryId);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+    
 }
--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -1,7 +1,41 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.repositories.CategoryRepository;
+import com.ecommerce.project.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+    
+    public ProductServiceImpl(
+        ProductRepository productRepository,
+        ModelMapper modelMapper,
+        CategoryRepository categoryRepository
+    ) {
+        this.productRepository = productRepository;
+        this.modelMapper = modelMapper;
+        this.categoryRepository = categoryRepository;
+    }
+    
+    @Override
+    public ProductDTO addProduct(ProductDTO productDTO, Long categoryId) {
+        Product product = modelMapper.map(productDTO, Product.class);
+        Category category = categoryRepository.findById(categoryId)
+                                .orElseThrow(() -> new ResourceNotFoundException("Category", "id", categoryId));
+                
+        product.setCategory(category);
+        
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
 }
