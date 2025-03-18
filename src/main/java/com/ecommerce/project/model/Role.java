1445435fa7d1c47354235ffaeafe60d93af6f51f
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-03-18 14:23:44 +0300

    Add RoleRepository and remove allargs lombok annotation to use local constructor in the Role entity

--- src/main/java/com/ecommerce/project/model/Role.java
+++ src/main/java/com/ecommerce/project/model/Role.java
@@ -8,11 +8,9 @@ import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Table(name = "roles")
 public class Role {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id")
@@ -22,7 +20,7 @@ public class Role {
     @Enumerated(EnumType.STRING)
     @Column(length = 20, name = "role_name")
     private AppRole roleName;
-
+    
     public Role(AppRole roleName) {
         this.roleName = roleName;
     }
