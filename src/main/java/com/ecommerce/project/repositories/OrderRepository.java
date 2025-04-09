43760094c38696bef3b6f6dbb222ed73642899a3
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-04-09 05:21:03 +0300

    Add OrderRepository interface for managing Order entities

---
+++ src/main/java/com/ecommerce/project/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.ecommerce.project.repositories;
+
+import com.ecommerce.project.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
