bfa4c70b2c8dfe0504cb2a8ece56784e08f1557a
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-04-09 05:17:58 +0300

    Add AuthUtil for user authentication utilities and update WebSecurityConfig to permit cart access

---
+++ src/main/java/com/ecommerce/project/utils/AuthUtil.java
@@ -0,0 +1,51 @@
+package com.ecommerce.project.utils;
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+    private final UserRepository userRepository;
+    
+    public AuthUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    public String loggedInEmail() {
+        Authentication authentication = getAuthenticationObj();
+        User user = userRepository.findByUserName(getAuthenticationObj().getName())
+            .orElseThrow(
+                () -> new UsernameNotFoundException("User with username: " + authentication.getName() + "Not Found")
+            );
+        
+        return user.getEmail();
+    }
+    
+    public Long loggedInUserId() {
+        Authentication authentication = getAuthenticationObj();
+        User user = userRepository.findByUserName(authentication.getName())
+            .orElseThrow(
+                () -> new UsernameNotFoundException("User with username: " + authentication.getName() + "Not Found")
+            );
+        
+        return user.getUserId();
+    }
+    
+    public User loggedInUser() {
+        Authentication authentication = getAuthenticationObj();
+        
+        User user = userRepository.findByUserName(authentication.getName()).orElseThrow(
+            () -> new UsernameNotFoundException("User with username: " + authentication.getName() + "Not Found")
+        );
+        
+        return user;
+    }
+    
+    private Authentication getAuthenticationObj() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
