db2de9ed4ea261595524a3b5d7c2d83a4bbfe81e
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-04-09 05:20:27 +0300

    Add CartItemDTO and CartItemRepository; update CartItem to use wrapper types for discount and productPrice

--- src/main/java/com/ecommerce/project/model/CartItem.java
+++ src/main/java/com/ecommerce/project/model/CartItem.java
@@ -24,6 +24,6 @@ public class CartItem {
     private Product product;
 
     private Integer quantity;
-    private double discount;
-    private double productPrice;
+    private Double discount;
+    private Double productPrice;
 }
