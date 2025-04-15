316d2e9d2bfc78487d3ed962a14fda8cce8f6a8f
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-04-15 15:30:59 +0300

    Refactor APIResponse: change message field to public and update status field to use primitive type for consistency

--- src/main/java/com/ecommerce/project/payload/APIResponse.java
+++ src/main/java/com/ecommerce/project/payload/APIResponse.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class APIResponse {
-    private String message;
-    private Boolean status;
+    public String message;
+    private boolean status;
 }
