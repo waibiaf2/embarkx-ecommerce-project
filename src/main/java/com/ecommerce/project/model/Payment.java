cbbb4f5fe97d059a414ad004f967a2a0560fcf5a
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-04-15 15:31:27 +0300

    Refactor APIResponse: change message field to public and update status field to use primitive type for consistency

--- src/main/java/com/ecommerce/project/model/Payment.java
+++ src/main/java/com/ecommerce/project/model/Payment.java
@@ -13,26 +13,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Payment {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long paymentId;
-
-    @OneToOne(mappedBy = "payment", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    
+    @OneToOne(mappedBy = "payment", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Order order;
-
+    
+    
     @NotBlank
     @Size(min = 4, message = "Payment method must contain at least 4 characters")
     private String paymentMethod;
-	
+    
     private String pgPaymentId;
     private String pgStatus;
     private String pgResponseMessage;
-
+    
     private String pgName;
-
-    public Payment(String paymentMethod, String pgPaymentId, String pgStatus,
-                   String pgResponseMessage, String pgName) {
+    
+    public Payment(
+        String paymentMethod,
+        String pgPaymentId,
+        String pgStatus,
+        String pgResponseMessage,
+        String pgName
+    ) {
         this.paymentMethod = paymentMethod;
         this.pgPaymentId = pgPaymentId;
         this.pgStatus = pgStatus;
