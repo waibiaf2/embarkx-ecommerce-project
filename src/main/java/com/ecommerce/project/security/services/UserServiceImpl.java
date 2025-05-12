ab40b83e148308000693b6b25051212bba110d57
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-12 17:19:55 +0300

    Refactor UserServiceImpl constructor and update .gitattributes
    Made the UserRepository field `final` to ensure immutability and thread safety. Updated .gitattributes to include additional configurations for consistent line endings and directory-specific settings.

--- src/main/java/com/ecommerce/project/security/services/UserServiceImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
     private final ModelMapper modelMapper;
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserServiceImpl(
         UserRepository userRepository,
